250f1f9f7a5e0e905849418368b345ba5c560e87
Sang Dang <sang@dang.to>
2024-02-09 12:24:43 +0200

    init

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,14 +1,69 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Spinner } from '@/components/client/Spinner';
+import { Slot } from '@radix-ui/react-slot';
+import { cn } from '@/lib/utils';
+import * as React from 'react';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        solid: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        outline:
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+      },
+      size: {
+        xs: 'h-8 rounded-sm px-2 text-xs',
+        sm: 'h-9 rounded px-3 text-sm',
+        md: 'h-10 rounded-md px-3 py-2 text-base',
+        lg: 'h-11 rounded-lg px-6 text-lg',
+        icon: 'h-8 w-8 p-0 rounded-full',
+      },
+    },
+    defaultVariants: {
+      variant: 'solid',
+      size: 'md',
+    },
+  }
+);
+
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  isLoading?: boolean;
 }
 
-export function Button({ children, ...other }: ButtonProps): JSX.Element {
-  return (
-    <button type="button" {...other}>
-      {children}
-    </button>
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      children,
+      isLoading,
+      variant,
+      size,
+      asChild = false,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        disabled={isLoading}
+        {...props}
+      >
+        {isLoading && <Spinner className="mr-2" />}
 
-Button.displayName = "Button";
+        {children}
+      </Comp>
+    );
+  }
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
